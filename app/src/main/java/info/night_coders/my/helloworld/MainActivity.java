9693463af682e54240d6eb300ce7a7c949709e53
faceless <msuprunov@daybyday.de>
2017-07-04 23:53:24 +0200

    added a simple hello-world project and extended gitignore file

---
+++ app/src/main/java/info/night_coders/my/helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package info.night_coders.my.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
